7bab2a6683ca072cb83e7770682ff88c184219a7
Avinash <avinash@goldceownlabs.com>
2026-02-10 17:30:06 +0530

    Update UI for Home Leads Tasks and Contact Profile All

--- app/(tabs)/tasks.tsx
+++ app/(tabs)/tasks.tsx
@@ -202,6 +202,7 @@ export default function TasksScreen() {
             formatDate={formatDate}
             getDaysUntilDue={getDaysUntilDue}
             colors={colors}
+            
           />
         </View>
 
